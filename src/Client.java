0349f77b6526a52df03ff43d527665ede03318cf
yermeki <yermek@inbox.ru>
2013-10-17 11:49:53 +0500

    second commit: grouping some lines

--- src/Client.java
+++ src/Client.java
@@ -1,11 +1,13 @@
+@SuppressWarnings({"UnusedDeclaration"})
 public class Client {
     public static void main(String[] ar) {
 
         model myForm = new model();
         myForm.setVisible(true);
+
         String txt = myForm.MyTextField.getText();
         System.out.println(txt);
-        //myController.address = myForm.MyTextField.getText();
+
         Controller myController = new Controller("127.0.0.1", "Hello Server!");
 
 
